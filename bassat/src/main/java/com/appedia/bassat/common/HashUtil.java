e36a61a1f9a015d260c4e9f31eff4ba1a5bef42a
Muz Omar <muzamilo@gmail.com>
2014-03-26 11:50:01 +0200

    New addition

---
+++ bassat/src/main/java/com/appedia/bassat/common/HashUtil.java
@@ -0,0 +1,88 @@
+package com.appedia.bassat.common;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Muz Omar
+ */
+public class HashUtil {
+
+    /**
+     *
+     * @param string
+     * @param algorithm
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    public static String hash(String string, String algorithm) throws UnsupportedEncodingException {
+        return hash(string, algorithm, Charset.defaultCharset().toString());
+    }
+
+    /**
+     *
+     * @param string
+     * @param algorithm
+     * @param encoding
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    public static String hash(String string, String algorithm, String encoding) throws UnsupportedEncodingException {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+
+            digest.update(string.getBytes(encoding));
+
+            byte[] encodedPassword = digest.digest();
+
+            return new BigInteger(1, encodedPassword).toString(16);
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     *
+     * @param file
+     * @param algorithm
+     * @return
+     * @throws IOException
+     */
+    public static String hash(File file, String algorithm)
+            throws IOException {
+
+        FileInputStream fileInputStream = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            fileInputStream = new FileInputStream(file);
+
+            DigestInputStream digestInputStream = new DigestInputStream(fileInputStream, digest);
+
+            byte[] buffer = new byte[8192];
+
+            int bytesRead = 1;
+            while ((bytesRead = digestInputStream.read(buffer)) != -1) { }
+
+            byte[] hash = digest.digest();
+
+            return new BigInteger(1, hash).toString(16);
+
+        } catch (NoSuchAlgorithmException e) {
+
+            throw new RuntimeException(e);
+
+        } finally {
+            fileInputStream.close();
+        }
+    }
+
+}
